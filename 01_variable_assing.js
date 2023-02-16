f6c2694cb20a8b917160b554ae2978fbfcefc8bd
dipikakhobragade <sk.dipika@gmail.com>
2023-02-16 17:16:56 +0530

    First  Program created with variable
    First  Program created with variable

---
+++ 01_variable_assing.js
@@ -0,0 +1,22 @@
+console.log("********************************Step 1*********************");
+var myFullName;
+console.log("Variable declaired but not Initialised value is:",myFullName);
+myFullName ="Dipika Khobragade";
+console.log("My full name is:",myFullName);
+myFullName ="Dipika Shamrao khobragade";
+console.log("My full name with Middle name is:",myFullName);
+ 
+console.log("*******************************Step 2***********************");
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+console.log("*******************************Step 3***********************");
+
+var firstName = "Dipika";
+var lastName = "Khobragade";
+var age = "36";
+var address = "Palava Phase 2, Casa Aurora A, Dombivli East,Thane";
+var pincode = "421204";
+var gender = "Female";
+var isMarried = "Yes";
+console.log("First Name :",firstName, "Last Name :",lastName,"Age :",age,"Address:", address,"Pincode :", pincode,"Gender: ",gender,"Married Status:",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,25 @@
-console.log(" Dipika Khobragade ");
+console.log("Dipika Khobragade");
+console.log("36");
+console.log("Mumbai");
+
+var fullName = ("Dipika Khobragade");
+console.log(fullName);
+console.log(fullName);
+
+var city; //variable Declaration
+console.log(city);
+city = "Mumbai"; //variable Initialization
+city = "pune";
+console.log(city);
+
+
+var age = "36";
+age = "37";
+age = "38";
+console.log(age);
+
+// var university;
+// university = "RTMNU";
+// console.log(university);
+var university = "RTMNU";
+console.log(university);
