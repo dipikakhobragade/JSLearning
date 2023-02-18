607aeeb0fa24e623cc1bcd2af9307a2615780ff1
dipikakhobragade <sk.dipika@gmail.com>
2023-02-18 09:26:40 +0530

    Created Swapping Variables File
    Created Swapping Variables File

---
+++ 02_swapVariables.js
@@ -0,0 +1,22 @@
+console.log("Swapping Two Variables");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log(sweety,cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log(sweety, cutie);
+console.log("-----------------------------------------------");
+
+console.log("Swapping Three Variables");
+var num1 =100;
+var num2 = 200;
+var num3 =300;
+console.log(num1,num2,num3);
+var temp= num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log(num1,num2,num3);
+
+
