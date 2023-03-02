5e5b63cde1120f536db761b1bc029a66838c1c9e
dipikakhobragade <sk.dipika@gmail.com>
2023-03-03 02:54:25 +0530

    created for loop assignment file
    created for loop assignment file

---
+++ 05_incrementDecrementOprter.js
@@ -0,0 +1,20 @@
+
+
+
+// index++ ====> index+1 post increment operator
+// index-- ====> index-1 post decrement
+//++ index ======> pre increment
+//-- index ======> pre decrement
+
+var index= 10;
+var y = index+1;
+console.log(index , y);
+
+var index= 20;
+var y = index++;
+console.log(index , y);
+
+
+var index= 20;
+var y = index--;
+console.log(index , y);
---
+++ 06_for_loop.js
@@ -0,0 +1,5 @@
+
+for (let index = 1; index <=10; index  = index+1) {
+    console.log(`print ${index} number`);
+    
+}
